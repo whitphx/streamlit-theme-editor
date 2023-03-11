7179c4c59586e83559e1b89c9f0aa595bdd096d7
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2023-03-11 18:13:54 +0900

    Add WCAG texts

--- fragments.py
+++ fragments.py
@@ -12,6 +12,13 @@ def contrast_summary(label: str, foreground_rgb_hex: str, background_rgb_hex: st
 
     st.metric(label, value=f"{contrast_ratio_str} : 1", label_visibility="collapsed")
 
+    if contrast.passes_AAA(contrast_ratio):
+        st.markdown(":white_check_mark: :white_check_mark: WCAG AAA")
+    elif contrast.passes_AA(contrast_ratio):
+        st.markdown(":white_check_mark: WCAG AA")
+    else:
+        st.markdown(":x: Fail WCAG")
+
     st.markdown(f'<p style="color: {foreground_rgb_hex}; background-color: {background_rgb_hex}; padding: 12px">Lorem ipsum</p>', unsafe_allow_html=True)
 
 

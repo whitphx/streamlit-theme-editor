d1d5d00daecc079525c6ab4ff53385d2cee318ce
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2023-03-11 18:06:07 +0900

    Refactoring the metric views

--- fragments.py
+++ fragments.py
@@ -4,14 +4,16 @@ import wcag_contrast_ratio as contrast
 import util
 
 
-def contrast_summary(foreground_rgb_hex: str, background_rgb_hex: str) -> None:
+def contrast_summary(label: str, foreground_rgb_hex: str, background_rgb_hex: str) -> None:
     rgb_foreground = util.parse_hex(foreground_rgb_hex)
     rgb_background = util.parse_hex(background_rgb_hex)
     contrast_ratio = contrast.rgb(rgb_foreground, rgb_background)
     contrast_ratio_str = f"{contrast_ratio:.2f}"
-    st.metric("", value=f"{contrast_ratio_str} : 1")
+
+    st.metric(label, value=f"{contrast_ratio_str} : 1", label_visibility="collapsed")
 
     st.markdown(f'<p style="color: {foreground_rgb_hex}; background-color: {background_rgb_hex}; padding: 12px">Lorem ipsum</p>', unsafe_allow_html=True)
+
 
 def sample_components(key: str):
     st.slider("Slider", min_value=0, max_value=100, key=f"{key}:slider")

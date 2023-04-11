c21a17b555028fffdc74f99f3cf89489942881af
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2023-04-11 17:21:38 +0900

    Remove unused function

--- app.py
+++ app.py
@@ -117,12 +117,6 @@ background_color = color_picker('Background color', key="backgroundColor", defau
 secondary_background_color = color_picker('Secondary background color', key="secondaryBackgroundColor", default_color=default_color.secondaryBackgroundColor, l_only=True)
 
 
-def parse_hex(rgb_hex_str: str) -> tuple[float, float, float]:
-    if not re.match(r"^#[0-9a-fA-F]{6}$", rgb_hex_str):
-        raise ValueError("Invalid hex color")
-    return tuple(int(rgb_hex_str[i:i+2], 16) / 255 for i in (1, 3, 5))
-
-
 st.header("WCAG contrast ratio")
 st.markdown("""
 Check if the color contrasts of the selected colors are enough to the WCAG guidelines recommendation.
